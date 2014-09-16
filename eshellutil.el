4f44cac19f726a350cd54ecf1eb2335af4a6d1f7
Syohei YOSHIDA <syohex@gmail.com>
2014-09-16 13:44:11 +0900

    initial implementation

---
+++ eshellutil.el
@@ -0,0 +1,57 @@
+;;; eshellutil.el --- My own Eshell utilities
+
+;; Copyright (C) 2014 by Syohei YOSHIDA
+
+;; Author: Syohei YOSHIDA <syohex@gmail.com>
+;; URL: https://github.com/syohex/
+;; Version: 0.01
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'eshell)
+
+(defvar eshellutil--previous-buffer nil)
+
+(defun eshellutil-shell-pop-up-hook ()
+  (setq eshellutil--previous-buffer (current-buffer)))
+
+(defun eshell/cde ()
+  (let* ((file-name (buffer-file-name eshellutil--previous-buffer))
+         (dir (or (and file-name (file-name-directory file-name))
+                  (and (eq major-mode 'dired-mode) dired-directory)
+                  (with-current-buffer eshellutil--previous-buffer
+                    default-directory))))
+    (eshell/cd dir)))
+
+(defun eshell/cdp ()
+  (let ((dir (cl-loop with cwd = default-directory
+                      for d in '(".git" ".hg" ".svn")
+                      when (locate-dominating-file cwd d)
+                      return (file-name-directory it))))
+    (eshell/cd dir)))
+
+(defun eshell/e (file)
+  (let ((curwin (get-buffer-window))
+        (filepath (concat default-directory file)))
+    (other-window 1)
+    (find-file filepath)
+    (delete-window curwin)))
+
+(provide 'eshellutil)
+
+;;; eshellutil.el ends here
